c8ada94d1fe257c1b85ace4009cb8caecf6f95d3
Merlin Patterson <merlinpatt@gmail.com>
2018-01-26 09:10:31 -0500

    Created basic form for test case editor

---
+++ imports/ui/components/TestCaseEditor/TestCaseEditor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestCaseEditor = () => {
+  return (
+    <form>
+      <input name="name" placeholder="name" />
+      <input name="type" placeholder="type" />
+    </form>
+  );
+};
+
+export default TestCaseEditor;
--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -27,6 +27,7 @@ import Footer from '../../components/Footer/Footer';
 import Terms from '../../pages/Terms/Terms';
 import Privacy from '../../pages/Privacy/Privacy';
 import ExamplePage from '../../pages/ExamplePage/ExamplePage';
+import TestCaseEditorPage from '../../pages/TestCaseEditorPage/TestCaseEditorPage';
 import VerifyEmailAlert from '../../components/VerifyEmailAlert/VerifyEmailAlert';
 import getUserName from '../../../modules/get-user-name';
 
@@ -61,6 +62,7 @@ const App = props => (
             <Route name="terms" path="/terms" component={Terms} />
             <Route name="privacy" path="/privacy" component={Privacy} />
             <Route name="examplePage" path="/example-page" component={ExamplePage} />
+            <Route path="/test-case-editor" component={TestCaseEditorPage} />
             <Route component={NotFound} />
           </Switch>
         </Grid>
