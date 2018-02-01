b2b580708743e50b3a014666d167c4f554367254
DESKTOP-FED8EL0\cku <cku@tere.tech>
2018-02-01 17:49:31 +0100

    Added additional fields

--- imports/ui/components/TestCaseEditor/TestCaseEditor.js
+++ imports/ui/components/TestCaseEditor/TestCaseEditor.js
@@ -4,7 +4,38 @@ const TestCaseEditor = () => {
   return (
     <form>
       <input name="name" placeholder="name" />
-      <input name="type" placeholder="type" />
+      <input name="loadingQueue" placeholder="Queue name" />
+      <input name="format" placeholder="format" />
+      <input name="messageType" placeholder="Message type" />
+      <textarea
+        className="form-control"
+        name="testMessage"
+        placeholder="test message"
+      />
+      <input
+        name="runTimeSec"
+        defaultValue="60"
+        placeholder="allowed runTime in Sec"
+      />
+      <textarea
+        className="form-control"
+        name="resultData"
+        placeholder="resultData"
+      />
+      <textarea
+        className="form-control"
+        name="rfh2Header"
+        placeholder="RFH2 MQ queue header value"
+      />
+      <textarea
+        className="form-control"
+        name="comment"
+        placeholder="comment"
+      />
+      <input name="group" placeholder="group" />
+      <input name="name" placeholder="name" />
+      <input name="completesInIpc" placeholder="completesInIpc" />
+      <input name="autoTest" placeholder="autoTest" />
     </form>
   );
 };
